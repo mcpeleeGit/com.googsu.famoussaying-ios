18fb2e610ebce049ae6daa088721507602366127
mcpeleeGit <mcpelee@gmail.com>
2025-03-15 21:31:20 +0900

    Initial Commit

---
+++ famoussaying/famoussayingApp.swift
@@ -0,0 +1,17 @@
+//
+//  famoussayingApp.swift
+//  famoussaying
+//
+//  Created by kakao on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct famoussayingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
